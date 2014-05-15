1e51eab5b5e4accf32a37de66e2e99a9f1a6c1dd
morgan95 <morganbuson28081995@gmail.com>
2014-05-15 15:26:59 +0200

    added subtabel Creature
    Signed-off-by: morgan95 <morganbuson28081995@gmail.com>

--- Luice/LuiceEditor.Designer.cs
+++ Luice/LuiceEditor.Designer.cs
@@ -127,6 +127,16 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.tabPage10 = new System.Windows.Forms.TabPage();
+            this.tabPage11 = new System.Windows.Forms.TabPage();
+            this.tabPage12 = new System.Windows.Forms.TabPage();
+            this.tabPage13 = new System.Windows.Forms.TabPage();
             this.gameobjectTab = new System.Windows.Forms.TabPage();
             this.questTab = new System.Windows.Forms.TabPage();
             this.itemTab = new System.Windows.Forms.TabPage();
@@ -155,7 +165,7 @@
             this.tabControl1.Location = new System.Drawing.Point(2, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1036, 500);
+            this.tabControl1.Size = new System.Drawing.Size(1114, 500);
             this.tabControl1.TabIndex = 0;
             // 
             // creatureTab
@@ -164,7 +174,7 @@
             this.creatureTab.Location = new System.Drawing.Point(4, 22);
             this.creatureTab.Name = "creatureTab";
             this.creatureTab.Padding = new System.Windows.Forms.Padding(3);
-            this.creatureTab.Size = new System.Drawing.Size(1028, 474);
+            this.creatureTab.Size = new System.Drawing.Size(1106, 474);
             this.creatureTab.TabIndex = 0;
             this.creatureTab.Text = "Creature";
             this.creatureTab.UseVisualStyleBackColor = true;
@@ -174,10 +184,20 @@
             this.tabControl2.Controls.Add(this.tabPage1);
             this.tabControl2.Controls.Add(this.tabPage2);
             this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Controls.Add(this.tabPage5);
+            this.tabControl2.Controls.Add(this.tabPage6);
+            this.tabControl2.Controls.Add(this.tabPage7);
+            this.tabControl2.Controls.Add(this.tabPage8);
+            this.tabControl2.Controls.Add(this.tabPage9);
+            this.tabControl2.Controls.Add(this.tabPage10);
+            this.tabControl2.Controls.Add(this.tabPage11);
+            this.tabControl2.Controls.Add(this.tabPage12);
+            this.tabControl2.Controls.Add(this.tabPage13);
             this.tabControl2.Location = new System.Drawing.Point(6, 3);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(1015, 465);
+            this.tabControl2.Size = new System.Drawing.Size(1093, 465);
             this.tabControl2.TabIndex = 0;
             // 
             // tabPage1
@@ -191,7 +211,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1007, 439);
+            this.tabPage1.Size = new System.Drawing.Size(1085, 439);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Search";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -225,9 +245,9 @@
             this.maxlevel,
             this.name_loc,
             this.subname_loc});
-            this.creatureSearchDgv.Location = new System.Drawing.Point(35, 69);
+            this.creatureSearchDgv.Location = new System.Drawing.Point(35, 66);
             this.creatureSearchDgv.Name = "creatureSearchDgv";
-            this.creatureSearchDgv.Size = new System.Drawing.Size(943, 329);
+            this.creatureSearchDgv.Size = new System.Drawing.Size(1036, 351);
             this.creatureSearchDgv.TabIndex = 3;
             // 
             // entry
@@ -272,7 +292,7 @@
             // 
             // findCreatureBtn
             // 
-            this.findCreatureBtn.Location = new System.Drawing.Point(903, 20);
+            this.findCreatureBtn.Location = new System.Drawing.Point(996, 20);
             this.findCreatureBtn.Name = "findCreatureBtn";
             this.findCreatureBtn.Size = new System.Drawing.Size(75, 23);
             this.findCreatureBtn.TabIndex = 2;
@@ -308,9 +328,9 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1007, 439);
+            this.tabPage2.Size = new System.Drawing.Size(1085, 439);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "creature_template";
+            this.tabPage2.Text = "Creature Template";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // label40
@@ -1028,17 +1048,117 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1007, 439);
+            this.tabPage3.Size = new System.Drawing.Size(1085, 439);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Creature Template Addons";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Creature Addon";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Creature Location";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Creature Loot";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Model Info";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Location = new System.Drawing.Point(4, 22);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage8.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage8.TabIndex = 7;
+            this.tabPage8.Text = "On Kill Reputation";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Location = new System.Drawing.Point(4, 22);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage9.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage9.TabIndex = 8;
+            this.tabPage9.Text = "Equip Template";
+            this.tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage10
+            // 
+            this.tabPage10.Location = new System.Drawing.Point(4, 22);
+            this.tabPage10.Name = "tabPage10";
+            this.tabPage10.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage10.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage10.TabIndex = 9;
+            this.tabPage10.Text = "Pickpocket Loot";
+            this.tabPage10.UseVisualStyleBackColor = true;
+            // 
+            // tabPage11
+            // 
+            this.tabPage11.Location = new System.Drawing.Point(4, 22);
+            this.tabPage11.Name = "tabPage11";
+            this.tabPage11.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage11.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage11.TabIndex = 10;
+            this.tabPage11.Text = "Skin Loot";
+            this.tabPage11.UseVisualStyleBackColor = true;
+            // 
+            // tabPage12
+            // 
+            this.tabPage12.Location = new System.Drawing.Point(4, 22);
+            this.tabPage12.Name = "tabPage12";
+            this.tabPage12.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage12.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage12.TabIndex = 11;
+            this.tabPage12.Text = "Involved in";
+            this.tabPage12.UseVisualStyleBackColor = true;
+            // 
+            // tabPage13
+            // 
+            this.tabPage13.Location = new System.Drawing.Point(4, 22);
+            this.tabPage13.Name = "tabPage13";
+            this.tabPage13.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage13.Size = new System.Drawing.Size(1085, 439);
+            this.tabPage13.TabIndex = 12;
+            this.tabPage13.Text = "Script";
+            this.tabPage13.UseVisualStyleBackColor = true;
             // 
             // gameobjectTab
             // 
             this.gameobjectTab.Location = new System.Drawing.Point(4, 22);
             this.gameobjectTab.Name = "gameobjectTab";
             this.gameobjectTab.Padding = new System.Windows.Forms.Padding(3);
-            this.gameobjectTab.Size = new System.Drawing.Size(1028, 474);
+            this.gameobjectTab.Size = new System.Drawing.Size(1106, 474);
             this.gameobjectTab.TabIndex = 2;
             this.gameobjectTab.Text = "GameObject";
             this.gameobjectTab.UseVisualStyleBackColor = true;
@@ -1048,7 +1168,7 @@
             this.questTab.Location = new System.Drawing.Point(4, 22);
             this.questTab.Name = "questTab";
             this.questTab.Padding = new System.Windows.Forms.Padding(3);
-            this.questTab.Size = new System.Drawing.Size(1028, 474);
+            this.questTab.Size = new System.Drawing.Size(1106, 474);
             this.questTab.TabIndex = 2;
             this.questTab.Text = "Quest";
             this.questTab.UseVisualStyleBackColor = true;
@@ -1058,7 +1178,7 @@
             this.itemTab.Location = new System.Drawing.Point(4, 22);
             this.itemTab.Name = "itemTab";
             this.itemTab.Padding = new System.Windows.Forms.Padding(3);
-            this.itemTab.Size = new System.Drawing.Size(1028, 474);
+            this.itemTab.Size = new System.Drawing.Size(1106, 474);
             this.itemTab.TabIndex = 3;
             this.itemTab.Text = "Item";
             this.itemTab.UseVisualStyleBackColor = true;
@@ -1068,7 +1188,7 @@
             this.smartaiTab.Location = new System.Drawing.Point(4, 22);
             this.smartaiTab.Name = "smartaiTab";
             this.smartaiTab.Padding = new System.Windows.Forms.Padding(3);
-            this.smartaiTab.Size = new System.Drawing.Size(1028, 474);
+            this.smartaiTab.Size = new System.Drawing.Size(1106, 474);
             this.smartaiTab.TabIndex = 4;
             this.smartaiTab.Text = "SmartAI";
             this.smartaiTab.UseVisualStyleBackColor = true;
@@ -1078,7 +1198,7 @@
             this.characterTab.Location = new System.Drawing.Point(4, 22);
             this.characterTab.Name = "characterTab";
             this.characterTab.Padding = new System.Windows.Forms.Padding(3);
-            this.characterTab.Size = new System.Drawing.Size(1028, 474);
+            this.characterTab.Size = new System.Drawing.Size(1106, 474);
             this.characterTab.TabIndex = 5;
             this.characterTab.Text = "Character";
             this.characterTab.UseVisualStyleBackColor = true;
@@ -1218,5 +1338,15 @@
         private System.Windows.Forms.Label label28;
         private System.Windows.Forms.Label label40;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage8;
+        private System.Windows.Forms.TabPage tabPage9;
+        private System.Windows.Forms.TabPage tabPage10;
+        private System.Windows.Forms.TabPage tabPage11;
+        private System.Windows.Forms.TabPage tabPage12;
+        private System.Windows.Forms.TabPage tabPage13;
     }
 }
--- Luice/goals.md
+++
@@ -1,8 +0,0 @@
-Goals:
-ToDo
-- Finish to rewrite Truice in C#
-- Customizations.
-- Add Mysql Ssh Connection
-
-Done:
-- Implement ssh Connection
